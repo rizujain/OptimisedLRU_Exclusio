ffc1b639a44685e14fde01d067d936a2a29297c4
Rizu Jain <rrizujain@gmail.com>
2019-11-29 17:40:25 -0600

    Reformat
    Signed-off-by: Rizu Jain <rrizujain@gmail.com>

--- replacement_state.cpp
+++ replacement_state.cpp
@@ -32,7 +32,6 @@ using namespace std;
 **
 */
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // The replacement state constructor:                                         //
 // Inputs: number of sets, associativity, and replacement policy to use       //
@@ -41,14 +40,14 @@ using namespace std;
 // DO NOT CHANGE THE CONSTRUCTOR PROTOTYPE                                    //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-CACHE_REPLACEMENT_STATE::CACHE_REPLACEMENT_STATE( UINT32 _sets, UINT32 _assoc, UINT32 _pol )
+CACHE_REPLACEMENT_STATE::CACHE_REPLACEMENT_STATE(UINT32 _sets, UINT32 _assoc, UINT32 _pol)
 {
 
-    numsets    = _sets;
-    assoc      = _assoc;
+    numsets = _sets;
+    assoc = _assoc;
     replPolicy = _pol;
 
-    mytimer    = 0;
+    mytimer = 0;
 
     InitReplacementState();
 }
@@ -58,17 +57,16 @@ CACHE_REPLACEMENT_STATE::CACHE_REPLACEMENT_STATE( UINT32 _sets, UINT32 _assoc, U
 // The function prints the statistics for the cache                           //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-ostream & CACHE_REPLACEMENT_STATE::PrintStats(ostream &out)
+ostream &CACHE_REPLACEMENT_STATE::PrintStats(ostream &out)
 {
 
-    out<<"=========================================================="<<endl;
-    out<<"=========== Replacement Policy Statistics ================"<<endl;
-    out<<"=========================================================="<<endl;
+    out << "==========================================================" << endl;
+    out << "=========== Replacement Policy Statistics ================" << endl;
+    out << "==========================================================" << endl;
 
     // CONTESTANTS:  Insert your statistics printing here
-    
-    return out;
 
+    return out;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -82,25 +80,26 @@ void CACHE_REPLACEMENT_STATE::InitReplacementState()
 {
     // Create the state for sets, then create the state for the ways
 
-    repl  = new LINE_REPLACEMENT_STATE* [ numsets ];
+    repl = new LINE_REPLACEMENT_STATE *[numsets];
 
     // ensure that we were able to create replacement state
 
     assert(repl);
 
     // Create the state for the sets
-    for(UINT32 setIndex=0; setIndex<numsets; setIndex++) 
+    for (UINT32 setIndex = 0; setIndex < numsets; setIndex++)
     {
-        repl[ setIndex ]  = new LINE_REPLACEMENT_STATE[ assoc ];
+        repl[setIndex] = new LINE_REPLACEMENT_STATE[assoc];
 
-        for(UINT32 way=0; way<assoc; way++) 
+        for (UINT32 way = 0; way < assoc; way++)
         {
             // initialize stack position (for true LRU)
-            repl[ setIndex ][ way ].LRUstackposition = way;
+            repl[setIndex][way].LRUstackposition = way;
         }
     }
 
-    if (replPolicy != CRC_REPL_CONTESTANT) return;
+    if (replPolicy != CRC_REPL_CONTESTANT)
+        return;
 
     // Contestants:  ADD INITIALIZATION FOR YOUR HARDWARE HERE
 }
@@ -112,20 +111,21 @@ void CACHE_REPLACEMENT_STATE::InitReplacementState()
 // index for the line being replaced.                                         //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-INT32 CACHE_REPLACEMENT_STATE::GetVictimInSet( UINT32 tid, UINT32 setIndex, const LINE_STATE *vicSet, UINT32 assoc, Addr_t PC, Addr_t paddr, UINT32 accessType, UINT32 accessSource ) {
+INT32 CACHE_REPLACEMENT_STATE::GetVictimInSet(UINT32 tid, UINT32 setIndex, const LINE_STATE *vicSet, UINT32 assoc, Addr_t PC, Addr_t paddr, UINT32 accessType, UINT32 accessSource)
+{
     // If no invalid lines, then replace based on replacement policy
-    if( replPolicy == CRC_REPL_LRU ) 
+    if (replPolicy == CRC_REPL_LRU)
     {
-        return Get_LRU_Victim( setIndex );
+        return Get_LRU_Victim(setIndex);
     }
-    else if( replPolicy == CRC_REPL_RANDOM )
+    else if (replPolicy == CRC_REPL_RANDOM)
     {
-        return Get_Random_Victim( setIndex );
+        return Get_Random_Victim(setIndex);
     }
-    else if( replPolicy == CRC_REPL_CONTESTANT )
+    else if (replPolicy == CRC_REPL_CONTESTANT)
     {
         // Contestants:  ADD YOUR VICTIM SELECTION FUNCTION HERE
-	return Get_My_Victim (setIndex);
+        return Get_My_Victim(setIndex);
     }
 
     // We should never here here
@@ -144,20 +144,20 @@ INT32 CACHE_REPLACEMENT_STATE::GetVictimInSet( UINT32 tid, UINT32 setIndex, cons
 // whether the line was a cachehit or not (cacheHit=true implies hit)         //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-void CACHE_REPLACEMENT_STATE::UpdateReplacementState( 
-    UINT32 setIndex, INT32 updateWayID, const LINE_STATE *currLine, 
-    UINT32 tid, Addr_t PC, UINT32 accessType, bool cacheHit, UINT32 accessSource )
+void CACHE_REPLACEMENT_STATE::UpdateReplacementState(
+    UINT32 setIndex, INT32 updateWayID, const LINE_STATE *currLine,
+    UINT32 tid, Addr_t PC, UINT32 accessType, bool cacheHit, UINT32 accessSource)
 {
     // What replacement policy?
-    if( replPolicy == CRC_REPL_LRU ) 
+    if (replPolicy == CRC_REPL_LRU)
     {
-        UpdateLRU( setIndex, updateWayID );
+        UpdateLRU(setIndex, updateWayID);
     }
-    else if( replPolicy == CRC_REPL_RANDOM )
+    else if (replPolicy == CRC_REPL_RANDOM)
     {
         // Random replacement requires no replacement state update
     }
-    else if( replPolicy == CRC_REPL_CONTESTANT )
+    else if (replPolicy == CRC_REPL_CONTESTANT)
     {
         // Contestants:  ADD YOUR UPDATE REPLACEMENT STATE FUNCTION HERE
         // Feel free to use any of the input parameters to make
@@ -178,25 +178,27 @@ void CACHE_REPLACEMENT_STATE::UpdateReplacementState(
 // while bottom of LRU stack is 'assoc-1'                                     //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-INT32 CACHE_REPLACEMENT_STATE::Get_LRU_Victim( UINT32 setIndex )
+INT32 CACHE_REPLACEMENT_STATE::Get_LRU_Victim(UINT32 setIndex)
 {
-	// Get pointer to replacement state of current set
+    // Get pointer to replacement state of current set
 
-	LINE_REPLACEMENT_STATE *replSet = repl[ setIndex ];
-	INT32   lruWay   = 0;
+    LINE_REPLACEMENT_STATE *replSet = repl[setIndex];
+    INT32 lruWay = 0;
 
-	// Search for victim whose stack position is assoc-1
+    // Search for victim whose stack position is assoc-1
 
-	for(UINT32 way=0; way<assoc; way++) {
-		if (replSet[way].LRUstackposition == (assoc-1)) {
-			lruWay = way;
-			break;
-		}
-	}
+    for (UINT32 way = 0; way < assoc; way++)
+    {
+        if (replSet[way].LRUstackposition == (assoc - 1))
+        {
+            lruWay = way;
+            break;
+        }
+    }
 
-	// return lru way
+    // return lru way
 
-	return lruWay;
+    return lruWay;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -204,10 +206,10 @@ INT32 CACHE_REPLACEMENT_STATE::Get_LRU_Victim( UINT32 setIndex )
 // This function finds a random victim in the cache set                       //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
-INT32 CACHE_REPLACEMENT_STATE::Get_Random_Victim( UINT32 setIndex )
+INT32 CACHE_REPLACEMENT_STATE::Get_Random_Victim(UINT32 setIndex)
 {
     INT32 way = (rand() % assoc);
-    
+
     return way;
 }
 
@@ -219,32 +221,37 @@ INT32 CACHE_REPLACEMENT_STATE::Get_Random_Victim( UINT32 setIndex )
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-void CACHE_REPLACEMENT_STATE::UpdateLRU( UINT32 setIndex, INT32 updateWayID )
+void CACHE_REPLACEMENT_STATE::UpdateLRU(UINT32 setIndex, INT32 updateWayID)
 {
-	// Determine current LRU stack position
-	UINT32 currLRUstackposition = repl[ setIndex ][ updateWayID ].LRUstackposition;
+    // Determine current LRU stack position
+    UINT32 currLRUstackposition = repl[setIndex][updateWayID].LRUstackposition;
 
-	// Update the stack position of all lines before the current line
-	// Update implies incremeting their stack positions by one
+    // Update the stack position of all lines before the current line
+    // Update implies incremeting their stack positions by one
 
-	for(UINT32 way=0; way<assoc; way++) {
-		if( repl[setIndex][way].LRUstackposition < currLRUstackposition ) {
-			repl[setIndex][way].LRUstackposition++;
-		}
-	}
+    for (UINT32 way = 0; way < assoc; way++)
+    {
+        if (repl[setIndex][way].LRUstackposition < currLRUstackposition)
+        {
+            repl[setIndex][way].LRUstackposition++;
+        }
+    }
 
-	// Set the LRU stack position of new line to be zero
-	repl[ setIndex ][ updateWayID ].LRUstackposition = 0;
+    // Set the LRU stack position of new line to be zero
+    repl[setIndex][updateWayID].LRUstackposition = 0;
 }
 
-INT32 CACHE_REPLACEMENT_STATE::Get_My_Victim( UINT32 setIndex ) {
-	// return first way always
-	return 0;
+INT32 CACHE_REPLACEMENT_STATE::Get_My_Victim(UINT32 setIndex)
+{
+    // return first way always
+    return 0;
 }
 
-void CACHE_REPLACEMENT_STATE::UpdateMyPolicy( UINT32 setIndex, INT32 updateWayID ) {
-	// do nothing
+void CACHE_REPLACEMENT_STATE::UpdateMyPolicy(UINT32 setIndex, INT32 updateWayID)
+{
+    // do nothing
 }
 
-CACHE_REPLACEMENT_STATE::~CACHE_REPLACEMENT_STATE (void) {
+CACHE_REPLACEMENT_STATE::~CACHE_REPLACEMENT_STATE(void)
+{
 }
